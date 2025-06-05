70f939dc3e45bb736bcc3fd4fc77958b2821afda
LotusFoundAnotherWorld <2636258590@qq.com>
2025-06-05 11:03:01 +0800

    add debounce function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,4 +11,37 @@ function getValue(arr: [], key: string, value: any, path = ''){
     return result
 }
 
-export { parseTime, getValue }
+/**
+ * 防抖函数
+ * @param {Function} fn - 目标函数
+ * @param {number} delay - 延迟时间(ms)
+ * @param {boolean} immediate - 是否立即执行
+ * @returns {Function} 防抖处理后的函数
+ */
+function debounce(fn, delay = 1000, immediate = false) {
+    let timer = null
+  
+    return function (...args) {
+      const context = this
+  
+      // 清除已有定时器
+      if (timer) clearTimeout(timer)
+  
+      // 立即执行模式
+      if (immediate) {
+        const callNow = !timer
+        timer = setTimeout(() => {
+          timer = null
+        }, delay)
+        if (callNow) fn.apply(context, args)
+      }
+      // 延迟执行模式
+      else {
+        timer = setTimeout(() => {
+          fn.apply(context, args)
+        }, delay)
+      }
+    }
+  }
+
+export { parseTime, getValue, debounce }
